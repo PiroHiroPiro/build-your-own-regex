5370c85a4618139f5e798716d86ee6f66acdce05
PiroHiroPiro <hirohiro11071107@gmail.com>
2018-02-04 23:19:43 +0900

    make search function

--- regex.js
+++ regex.js
@@ -6,13 +6,25 @@ function matchOne(pattern, text) {
 }
 
 function search(pattern, text) {
-
+    if(pattern[0] === "^") {
+        return match(pattern.slice(1), text);
+    }else{
+        flag = true;
+        for(var index = 0; index < text.length; index++){
+            flag = flag && match(pattern, text.slice(index));
+        }
+        return flag
+    }
 }
 
 function match(pattern, text) {
-    if(!pattern) return true;
-    if(pattern === "$" && text === "") return true;
-    else return matchOne(pattern[0], text[0]) && match(pattern.slice(1), text.slice(1));
+    if(!pattern) {
+        return true;
+    }else if(pattern === "$" && text === ""){
+        return true;
+    }else{
+        return matchOne(pattern[0], text[0]) && match(pattern.slice(1), text.slice(1));
+    }
 }
 
 

38d510ef621710e14f13fddfb5abe5fc6331f871
PiroHiroPiro <hirohiro11071107@gmail.com>
2018-02-04 22:54:47 +0900

    make matchOne function

--- regex.js
+++ regex.js
@@ -1,5 +1,8 @@
 function matchOne(pattern, text) {
-
+    if (!pattern) return true;
+    if (!text) return false;
+    if (pattern === ".") return true;
+    return pattern === text;
 }
 
 function search(pattern, text) {

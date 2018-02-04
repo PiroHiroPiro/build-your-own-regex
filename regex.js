208d35004b6898f18ccf2ef5904f518bc7926003
PiroHiroPiro <hirohiro11071107@gmail.com>
2018-02-05 00:17:21 +0900

    add matchQuestion function

--- regex.js
+++ regex.js
@@ -17,11 +17,17 @@ function search(pattern, text) {
     }
 }
 
+function matchQuestion(pattern, text) {
+    return (matchOne(pattern[0], text[0]) && match(pattern.slice(2), text.slice(1))) || match(pattern.slice(2), text);
+}
+
 function match(pattern, text) {
     if(!pattern) {
         return true;
     }else if(pattern === "$" && text === ""){
         return true;
+    }else if(pattern[1] === "?"){
+        matchQuestion(pattern, text);
     }else{
         return matchOne(pattern[0], text[0]) && match(pattern.slice(1), text.slice(1));
     }
